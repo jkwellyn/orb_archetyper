966df181bb4327f66d3af587d06a2d0acd8f80f6
John Murphy <jackie.murphy@opower.com>
2014-07-01 09:21:14 -0400

    AUTO-199: whitespace cleanup.

--- lib/helpers/file_utils.rb
+++ lib/helpers/file_utils.rb
@@ -3,19 +3,19 @@ require 'ansi/code'
 # Helper Utilities for File and Directory Management
 class FileUtility
 
-	# Create a directory
-	def self.dir_creator(dir_path)
-	  Dir::mkdir(dir_path)
-	  puts "\t" + ANSI.green{"created"} + " #{dir_path}"
-	  return dir_path
-	end
+  # Create a directory
+  def self.dir_creator(dir_path)
+    Dir::mkdir(dir_path)
+    puts "\t" + ANSI.green{"created"} + " #{dir_path}"
+    return dir_path
+  end
 
-	#creates file, requires dir to exist
-	def self.file_creator(dir_path, file_name, file_contents)
-	  file_dir = File.join(dir_path,file_name)
-	  file = open(file_dir, "w")
-	  file.write(file_contents)
-	  file.close
-	  puts "\t" + ANSI.green{"created"} + " #{file_dir}"
-	end
+  #creates file, requires dir to exist
+  def self.file_creator(dir_path, file_name, file_contents)
+    file_dir = File.join(dir_path,file_name)
+    file = open(file_dir, "w")
+    file.write(file_contents)
+    file.close
+    puts "\t" + ANSI.green{"created"} + " #{file_dir}"
+  end
 end
--- lib/option_parser.rb
+++ lib/option_parser.rb
@@ -1,83 +1,83 @@
 require 'optparse'
 require_relative 'orb-archetyper/version'
 
-# read options from command line and return as hash 
+# read options from command line and return as hash
 class OptionParser
 
-	# Parse the command line args
-	def self.parse(args)
+  # Parse the command line args
+  def self.parse(args)
 
-		options = {}
+    options = {}
 
-		options[:github] = false
-		
-		opt_parser = OptionParser.new do |opts|
-			opts.separator "\n"
-			opts.banner = "Orb Archetype Generator:"
-	
-			opts.separator "Usage: orb-archetyper [OPTIONS]"
-			opts.separator "Specific options:"
+    options[:github] = false
 
-			# list of choices
+    opt_parser = OptionParser.new do |opts|
+      opts.separator "\n"
+      opts.banner = "Orb Archetype Generator:"
+
+      opts.separator "Usage: orb-archetyper [OPTIONS]"
+      opts.separator "Specific options:"
+
+      # list of choices
       # TODO print template options here
-			opts.on("-t", "--type [TYPE]",
-				"Select project type (cli, core, utility, test).") do |t|
-					options[:type] = t
-			end 
+      opts.on("-t", "--type [TYPE]",
+        "Select project type (cli, core, utility, test).") do |t|
+          options[:type] = t
+      end
 
-			opts.on("-p", "--project [STRING]", "Specify the project name.") do |p|
-				options[:project] = p
-			end
+      opts.on("-p", "--project [STRING]", "Specify the project name.") do |p|
+        options[:project] = p
+      end
 
-			opts.separator ""
-			opts.separator "Configuration options:"
+      opts.separator ""
+      opts.separator "Configuration options:"
 
-			opts.on("-x", "--exclude x,y,z", Array, "Explicitly state the files/folders you wish to EXCLUDE from the archetype.") do |x|
-				options[:exclude] = x
-			end
+      opts.on("-x", "--exclude x,y,z", Array, "Explicitly state the files/folders you wish to EXCLUDE from the archetype.") do |x|
+        options[:exclude] = x
+      end
 
-			opts.on("-i", "--include x,y,z", Array, "Explicitly state the files/folders you wish to INCLUDE into the archetype.") do |i|
-				options[:include] = i
-			end
+      opts.on("-i", "--include x,y,z", Array, "Explicitly state the files/folders you wish to INCLUDE into the archetype.") do |i|
+        options[:include] = i
+      end
 
-			# optional arguement, true if declared
-			opts.on("-g", "--[no-]github", "Create the git repo for the project,", 
-				"False if omitted, true if declared.") do |g|
-					unless g.nil?
-						options[:github] = g
-					end
-			end
+      # optional arguement, true if declared
+      opts.on("-g", "--[no-]github", "Create the git repo for the project,", 
+        "False if omitted, true if declared.") do |g|
+          unless g.nil?
+            options[:github] = g
+          end
+      end
 
-			opts.separator ""
-			opts.separator "Helper options:"
+      opts.separator ""
+      opts.separator "Helper options:"
 
       #TODO fix this
-			# display file/folder components
-			#opts.on_tail("-e", "--expand", "Display file/folder choices.") do
-		 	#	puts TemplateManager.new(options[:project],options[:project]).subtypes
-		 	#	exit
-			#end
+      # display file/folder components
+      #opts.on_tail("-e", "--expand", "Display file/folder choices.") do
+      # puts TemplateManager.new(options[:project],options[:project]).subtypes
+      # exit
+      #end
 
-			# display version
-			opts.on_tail("-v", "--version", "Display version") do
-		 		puts OrbArchetyper::VERSION
-		 		exit
-			end
+      # display version
+      opts.on_tail("-v", "--version", "Display version") do
+        puts OrbArchetyper::VERSION
+        exit
+      end
 
-			# display help
-			opts.on_tail("-h","--help", "Show help") do
-		 		puts "For full documentation: https://github.va.opower.it/euan-davidson/orb-archetyper/blob/master/README.md"
-		 		puts  opts
-		 		exit
-			end
-		end
-		
-		opt_parser.parse(args)
+      # display help
+      opts.on_tail("-h","--help", "Show help") do
+        puts "For full documentation: https://github.va.opower.it/euan-davidson/orb-archetyper/blob/master/README.md"
+        puts  opts
+        exit
+      end
+    end
 
-		# raise an exception if we have not found a project name option or type is unsupported
-		raise OptionParser::MissingArgument if options[:project].nil?
-		raise OptionParser::InvalidArgument if options[:type].nil?
+    opt_parser.parse(args)
 
-		return options
-	end
+    # raise an exception if we have not found a project name option or type is unsupported
+    raise OptionParser::MissingArgument if options[:project].nil?
+    raise OptionParser::InvalidArgument if options[:type].nil?
+
+    return options
+  end
 end
