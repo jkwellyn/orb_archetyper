26092a21b2af876b556226a54a9eb8729875cbc9
adrian.cazacu <adrian.cazacu@opower.com>
2015-02-20 11:27:49 -0800

    AUTO-517: removed meta project related stuff

--- lib/cmd/generate.rb
+++ lib/cmd/generate.rb
@@ -10,7 +10,7 @@ class OrbCLI < Thor
                 aliases: ['-t'],
                 required: true,
                 default: 'core',
-                enum: %w(bertha_test cli core meta test),
+                enum: %w(bertha_test cli core test),
                 desc: 'Project type'
 
   # optional
--- lib/orb_archetyper/version.rb
+++ lib/orb_archetyper/version.rb
@@ -1,3 +1,3 @@
 module OrbArchetyper
-  VERSION = '1.4.1'
+  VERSION = '1.4.2'
 end
